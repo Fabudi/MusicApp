4e2afaa53fb7b0e31c8234a84da676c42283ed8e
Fabudi <evgeniydodg@gmail.com>
2024-03-12 00:48:09 +0300

    Library screen populated with some basic things

--- app/src/main/java/inc/fabudi/musicapp/ui/screens/LibraryScreen.kt
+++ app/src/main/java/inc/fabudi/musicapp/ui/screens/LibraryScreen.kt
@@ -2,24 +2,50 @@ package inc.fabudi.musicapp.ui.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import inc.fabudi.musicapp.ui.components.PlaylistCardWithNameInside
+import inc.fabudi.musicapp.ui.components.PlaylistCardWithNameInsidePreview
 
 @Composable
 fun LibraryScreen() {
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .background(color = MaterialTheme.colorScheme.background)
+            .padding(top = 16.dp)
     ) {
         Text(
-            text = "Library",
-            style = TextStyle(fontSize = 40.sp, color = MaterialTheme.colorScheme.primary)
+            text = "Library", style = TextStyle(
+                fontSize = 36.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onBackground
+            ), modifier = Modifier.padding(start = 16.dp)
         )
+        LazyVerticalStaggeredGrid(
+            columns = StaggeredGridCells.Fixed(2)
+        ){
+            items(30){
+                PlaylistCardWithNameInside()
+            }
+        }
     }
+}
+
+@Preview
+@Composable
+fun LibraryScreenPreview() {
+    LibraryScreen()
 }
