d1f2a2610fb37f696d2d6a21da8407f100624074
Fabudi <evgeniydodg@gmail.com>
2024-04-02 21:32:27 +0300

    Append User-Agent header to urls used for Glide

--- app/src/main/java/inc/fabudi/musicapp/Utils.kt
+++ app/src/main/java/inc/fabudi/musicapp/Utils.kt
@@ -5,5 +5,11 @@ import com.bumptech.glide.load.model.LazyHeaders
 
 object Utils {
 
+    fun String.toUrlWithUserAgent() = GlideUrl(
+        this, LazyHeaders.Builder().setHeader(
+            "User-Agent",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
+        ).build()
+    )
 
 }
