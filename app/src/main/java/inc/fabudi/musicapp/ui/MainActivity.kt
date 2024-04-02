33ab183ed7c004c95ab6d62c8585a197def37c4a
Fabudi <evgeniydodg@gmail.com>
2024-04-02 23:45:14 +0300

    Adding ViewModel and Hilt

--- app/src/main/java/inc/fabudi/musicapp/ui/MainActivity.kt
+++ app/src/main/java/inc/fabudi/musicapp/ui/MainActivity.kt
@@ -1,12 +1,17 @@
-package inc.fabudi.musicapp
+package inc.fabudi.musicapp.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import inc.fabudi.musicapp.viewmodel.MusicViewModel
 import inc.fabudi.musicapp.ui.screens.MainScreen
 import inc.fabudi.musicapp.ui.theme.MusicAppTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    val viewmodel: MusicViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
