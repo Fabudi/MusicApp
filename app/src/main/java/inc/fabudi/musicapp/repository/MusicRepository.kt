37fed3b9fbaf9bc6f0246f577698efefdb0eb1c6
Fabudi <evgeniydodg@gmail.com>
2024-04-02 23:36:04 +0300

    Basic Music Repo

---
+++ app/src/main/java/inc/fabudi/musicapp/repository/MusicRepository.kt
@@ -0,0 +1,8 @@
+package inc.fabudi.musicapp.repository
+
+import inc.fabudi.musicapp.network.MusicApiService
+import javax.inject.Inject
+
+class MusicRepository @Inject constructor(private val apiService: MusicApiService) {
+
+}
