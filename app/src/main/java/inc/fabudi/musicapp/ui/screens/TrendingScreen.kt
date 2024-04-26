9d0993e5ae92d2649cac8c674b6c899f7a112ca2
Fabudi <evgeniydodg@gmai.com>
2024-04-26 17:11:27 +0300

    Replace raw concatenation with function

--- app/src/main/java/inc/fabudi/musicapp/ui/screens/TrendingScreen.kt
+++ app/src/main/java/inc/fabudi/musicapp/ui/screens/TrendingScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import inc.fabudi.musicapp.Utils.toCommaString
 import inc.fabudi.musicapp.ui.components.GenreChipButton
 import inc.fabudi.musicapp.ui.components.TrendingTrackCard
 import inc.fabudi.musicapp.ui.theme.Typography
@@ -113,7 +114,7 @@ fun TrendingScreen(navController: NavHostController, viewmodel: MusicViewModel) 
                     onClick = {},
                     artworkUrl = track.artworkUrl,
                     title = track.title,
-                    author = track.authors.joinToString(", ") { it.nickname },
+                    author = track.authors.toCommaString(),
                     playsCount = track.playCount,
                     place = index + 1
                 )
