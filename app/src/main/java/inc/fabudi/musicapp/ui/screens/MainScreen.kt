1ded6764a722dbf9a2c10f20b9ad5811590477fc
Fabudi <evgeniydodg@gmai.com>
2024-04-26 17:57:52 +0300

    BottomSheet instead of new screen for Player

--- app/src/main/java/inc/fabudi/musicapp/ui/screens/MainScreen.kt
+++ app/src/main/java/inc/fabudi/musicapp/ui/screens/MainScreen.kt
@@ -2,24 +2,42 @@ package inc.fabudi.musicapp.ui.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.Scaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
-import inc.fabudi.musicapp.R
 import inc.fabudi.musicapp.ui.components.FBottomAppBar
 import inc.fabudi.musicapp.ui.components.PlayBar
 import inc.fabudi.musicapp.ui.navigation.NavigationGraph
 import inc.fabudi.musicapp.viewmodel.MusicViewModel
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(viewModel: MusicViewModel) {
     val navController = rememberNavController()
+    val bottomSheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = true
+    )
+    val scope = rememberCoroutineScope()
+    var openBottomSheet by rememberSaveable { mutableStateOf(false) }
+    val currentlyPlayingState = viewModel.player.currentlyPlaying.observeAsState()
+
     Scaffold(
         bottomBar = {
             Column(
@@ -33,7 +51,7 @@ fun MainScreen(viewModel: MusicViewModel) {
                             .fillMaxWidth()
                             .padding(bottom = 16.dp), onClick = {
                             openBottomSheet = true
-                            scope.launch { modalSheetState.show() }
+                            scope.launch { bottomSheetState.show() }
                         },
                         viewModel = viewModel
                     )
@@ -42,10 +60,21 @@ fun MainScreen(viewModel: MusicViewModel) {
             }
         }
     ) {
-        Box(modifier = Modifier
-            .fillMaxSize()
-            .padding(it)) {
-            NavigationGraph(navController = navController)
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            NavigationGraph(navController = navController, viewModel = viewModel)
+        }
+    }
+    if (openBottomSheet) {
+        ModalBottomSheet(
+            onDismissRequest = { openBottomSheet = false },
+            sheetState = bottomSheetState,
+            windowInsets = WindowInsets(0)
+        ) {
+            Player()
         }
     }
 }
